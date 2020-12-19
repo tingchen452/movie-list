00d6dffca46968492baa01a36fa0cdfd5472182f
Ting <tingcchen2@gmail.com>
2020-12-19 02:58:52 -0500

    Updates

---
+++ precache-manifest.e786d57653d2070d838020008b841dd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a001d0315c836bc0b11705cbd24d1555",
+    "url": "/movie-list/index.html"
+  },
+  {
+    "revision": "34caa799b58c322ed510",
+    "url": "/movie-list/static/css/main.d7ab9101.chunk.css"
+  },
+  {
+    "revision": "a8c71da180ebcbd7be9b",
+    "url": "/movie-list/static/js/2.3e30d47f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-list/static/js/2.3e30d47f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34caa799b58c322ed510",
+    "url": "/movie-list/static/js/main.fbc0b52b.chunk.js"
+  },
+  {
+    "revision": "5b27bad34ed5ed73a6b6",
+    "url": "/movie-list/static/js/runtime-main.ce696270.js"
+  }
+]);
